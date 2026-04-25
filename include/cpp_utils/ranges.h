a8a1225d236cc7bf9e13c09e39e1f8354d20393a
igor <igor>
2026-04-25 09:44:11 +0200

    Making it CMake compatible.

---
+++ include/cpp_utils/ranges.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+// This file provides a C++20 ranges adaptor to convert a range into a container, e.g. std::vector, std::list, etc.
+// Windows polyfill only
+
+#ifdef __WXMSW__
+
+namespace std::ranges
+{
+#include <vector>
+#include <ranges>
+#include <utility>
+
+    template <template<class...> class Container>
+    struct to_closure
+    {
+        template <std::ranges::input_range R>
+        auto operator()(R&& r) const
+        {
+            using T = std::ranges::range_value_t<R>;
+            Container<T> out;
+
+            if constexpr (std::ranges::sized_range<R> && requires(Container<T> c, std::size_t n) { c.reserve(n); })
+                out.reserve(std::ranges::size(r));
+
+            for (auto&& x : r)
+                out.push_back(std::forward<decltype(x)>(x));
+
+            return out;
+        }
+    };
+
+    template <template<class...> class Container>
+    constexpr auto to()
+    {
+        return to_closure<Container>{};
+    }
+
+    template <std::ranges::input_range R, template<class...> class Container>
+    auto operator|(R&& r, to_closure<Container> closure)
+    {
+        return closure(std::forward<R>(r));
+    }
+}
+
+#endif
