e039874ee4101de11df46022193d18c466e9a646
igor <igor>
2026-04-29 13:57:58 +0200

    Making it CMake compatible.

---
+++ include/cpp_utils/wx_string_format.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+template<>
+struct std::formatter<wxString, char> : std::formatter<std::string, char>
+{
+    auto format(const wxString& value, std::format_context& ctx) const
+    {
+        auto utf8 = value.ToUTF8();
+
+        std::string s = utf8.data()
+            ? std::string(utf8.data())
+            : std::string{};
+
+        return std::formatter<std::string, char>::format(s, ctx);
+    }
+};
+
+template<>
+struct std::formatter<wxString, wchar_t> : std::formatter<std::wstring, wchar_t>
+{
+    auto format(const wxString& value, std::wformat_context& ctx) const
+    {
+        return std::formatter<std::wstring, wchar_t>::format(
+            value.ToStdWstring(),
+            ctx
+        );
+    }
+};
